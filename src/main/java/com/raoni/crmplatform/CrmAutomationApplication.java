10b13a53259cdaed89a8299b8d5b3fbeca55ab0b
Raoni Medeiros <62862446+Raoni022@users.noreply.github.com>
2026-04-18 16:19:38 -0300

    Add Spring Boot application entry point

---
+++ src/main/java/com/raoni/crmplatform/CrmAutomationApplication.java
@@ -0,0 +1,11 @@
+package com.raoni.crmplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmAutomationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CrmAutomationApplication.class, args);
+    }
+}
